af2254147284ad6586f6c0639c7d445a057d5374
박기우 <KWiOS0101@gmail.com>
2024-11-25 21:15:45 +0900

    Feat: App1 프로젝트 생성

---
+++ FastAPP1/FastAPP1/FastAPP1App.swift
@@ -0,0 +1,17 @@
+//
+//  FastAPP1App.swift
+//  FastAPP1
+//
+//  Created by PKW on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FastAPP1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
