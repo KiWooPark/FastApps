2b4b9e3e76d510f09f99ebb39dcb53e76e7e8184
박기우 <KWiOS0101@gmail.com>
2024-12-09 19:43:43 +0900

    Feat: 프로젝트 생성
    - 타겟 15.0으로 설정

---
+++ FastApp2/FastApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastAPP2
+//
+//  Created by PKW on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
